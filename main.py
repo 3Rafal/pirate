1c8d13509fb531e64655d25513e98a1c2c482f80
Rafal Gwozdzinski <rafal@gwozdzinski.com>
2025-09-25 12:20:59 +0200

    remove useless cors

--- main.py
+++ main.py
@@ -6,14 +6,6 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
 app.mount("/static", StaticFiles(directory="static"), name="static")
 @app.get("/", response_class=FileResponse)
 async def read_root():
